2e72cf1b0e217d641057bdad277e0416c010290e
harrylloyd-bl <harry.lloyd@bl.uk>
2024-01-26 12:24:21 +0000

    ready for use
    read last processed from logs
    handle not snet machine tags
    use sqlite db to store tags
    simplify check RR for tags
    improve logging completeness

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import logging
 import re
-import glob
+import sqlite3
 import pickle
 from datetime import datetime
+import sys
 
 import flickr_api as fa
 import pandas as pd
@@ -10,6 +11,7 @@ from tqdm import tqdm
 
 from src.utils import start_loggers
 
+print("Initialising loggers")
 today = datetime.now().strftime("%y%m%d")
 complete_log = f"logs\\{today}_debug.log"
 progress_log = f"logs\\progress.log"
@@ -17,14 +19,11 @@ progress_log = f"logs\\progress.log"
 # All logging statements go to complete_log, only logging.info statements go to progress_log
 start_loggers(complete_log, progress_log)
 
+print("Reading research repository tags")
 # lookup tables
 category_tags = pd.read_csv("data\\external\\sherlocknet_tags_csv\\sherlock_flickr_tags.csv", index_col="flickr_id")
-rr_cat_tag_pkls = glob.glob("data\\external\\sherlocknet-tags\\tags\\*.pkl")  # rr = BL research repository
-combined_rr_tag_dct = {}
-for p in rr_cat_tag_pkls:
-    with open(p, "rb") as f:
-        tag_dct = pickle.load(f)
-        combined_rr_tag_dct |= tag_dct
+with open("data\\external\\sherlocknet-tags\\tags\\combined_final.pkl", "rb") as f:
+    combined_rr_tag_dct = pickle.load(f)
 
 
 def parse_tags(tag_str, tag_re):
@@ -35,69 +34,90 @@ def parse_tags(tag_str, tag_re):
     tag_list = tag_str.split(" ")
     res = {"category": [None], "tag": []}
     for t in tag_list:
-        snet, tag_type, text = tag_re.match(t).groupdict().values()
-        if snet != "sherlocknet:" or tag_type[:-1] not in ["category", "tag"]:
-            raise ValueError("Unknown machine tag type")
+        if tag_re.match(t):
+            ns, predicate, text = tag_re.match(t).groupdict().values()
+            if ns == "sherlocknet:" and predicate[:-1] in ["category", "tag"]:
+                res[predicate[:-1]].append(text)
+            else:
+                continue
         else:
-            res[type[:-1]].append(text)
+            continue
 
     return res["category"].pop(), res["tag"]
 
 
 # Get BL images
 # *** This auth has WRITE permissions, take care ***
+print("Authenticating and getting photo walker")
+with open("logs\\progress.log") as f:
+    prog = f.readlines()
+    clean_prog = [p.replace("  ", "").strip("\n").split(" ") for p in prog if "Processing" in p]
+    completed = sorted(list(set([int(p[6]) for p in clean_prog if p[3] == "INFO"])))
+    start_page = 1 + (max(completed) // 500)
+    start_id = (max(completed) // 500) * 500
+
 fa.set_auth_handler("flickr_api_session_auth_w.txt")
 user = fa.Person(id='12403504@N02')  # BL id
-photo_walker = fa.Walker(user.getPhotos, extras="machine_tags")
+# Either use Walker(page, per_page) or slice Walker to start at correct id.
+# Walker slicing is badly implemented so page/per page is easier
+photo_walker = fa.Walker(user.getPhotos, extras="machine_tags", page=start_page, per_page=500)
+
 tag_re = re.compile(r"(?P<snet>[a-z]*:)(?P<type>[a-z]*=)(?P<text>[a-z]*(?=\Z))")
 
-flkr_snet_cats = ["organism", "mammal", "landscapes", "architecture", "nature",
-                  "objects", "maps", "diagrams", "seals", "text", "decorations", "miniatures"]
+# tag and cat_map storage
+con = sqlite3.connect("data\\processed\\sherlocknet_tags.db")
+cur = con.cursor()
 
-snet_tags_from_flkr = {k: {} for k in flkr_snet_cats}
-flkr_rr_cat_mapping = {}
-
-for i, p in tqdm(enumerate(photo_walker[:5]), total=len(photo_walker)):
-    logging.info(f"Processing # {i} {p.id}")  # i is the (stable) position in the list of BL images
+start, end = max(completed) - start_id, None
+sl = slice(start, end)
+print("Running through photo walker")
+for i, p in tqdm(enumerate(photo_walker[sl]), total=len(photo_walker)):
     if not p.machine_tags:  # skip any that don't have machine tags
+        logging.info(f"Processing # {i + start_id + start} {p.id}")  # i is the (stable) position in the list of BL images
         logging.debug(f"No machine tags for {p.id}")
     else:
+        logging.info(f"Processing # {i + start_id + start} {p.id} T")  # i is the (stable) position in the list of BL images
         try:  # rr = BL research repository
             rr_cat, image_idx = category_tags.loc[int(p.id), ["tag", "image_idx"]]
         except KeyError:
             rr_cat, image_idx = None, None
-        rr_tags_exist = image_idx in combined_rr_tag_dct
-        logging.debug(f"IRO tags exist for {p.id}")
+
+        rr_tags = [x[0] for x in combined_rr_tag_dct.get(image_idx, [[None]])]
+        logging.debug(f"{p.id} has {len(rr_tags)} IRO tags")
 
         flkr_cat, flkr_tags = parse_tags(p.machine_tags, tag_re)
         logging.debug(f"{p.id} has {len(flkr_tags)} machine tags")
 
+        unmatched_tags = list(set(flkr_tags) - set(rr_tags))
+
         # Compare Flickr with RR to see if need to save any tags
-        if flkr_cat and rr_cat:  # tags on Flickr and in the RR
-            if flkr_cat != rr_cat:
-                flkr_rr_cat_mapping[image_idx] = [flkr_cat, rr_cat]
+        if flkr_cat != rr_cat:  # tags on Flickr and in the RR
+            print(f"\n{p.id} Flickr category {flkr_cat} doesn't match RR {rr_cat}")
+            logging.debug(f"{p.id} Flickr category {flkr_cat} doesn't match RR {rr_cat}")
+            data = [int(p.id), int(image_idx), flkr_cat, rr_cat]
+            try:
+                cur.execute("INSERT OR IGNORE INTO catmap VALUES (?, ?, ?, ?)", data)
+            except sqlite3.IntegrityError:
+                logging.debug("Already stored")
+                pass
+            con.commit()
 
-            unmatched_tags = list(set(flkr_tags) - set([x[0] for x in combined_rr_tag_dct.get(image_idx, ["empty_set"])]))
-            if rr_tags_exist and unmatched_tags:
-                logging.debug(f"{len(unmatched_tags)} Flickr tags not in RR, saving")
-                snet_tags_from_flkr[flkr_cat][image_idx] = unmatched_tags
-            elif not rr_tags_exist:
-                logging.debug(f"All Flickr tags not in RR, saving")
-                snet_tags_from_flkr[flkr_cat][image_idx] = unmatched_tags
-
-        elif flkr_cat and not rr_cat:  # tags on Flickr but not in RR
-            logging.debug(f"All Flickr tags not in RR, saving")
-            unmatched_tags = list(set(flkr_tags) - set([x[0] for x in combined_rr_tag_dct.get(image_idx, ["empty_set"])]))
-            snet_tags_from_flkr[flkr_cat][image_idx] = unmatched_tags
+        if unmatched_tags:
+            print(f"\n{p.id} has {len(unmatched_tags)} Flickr tags not in RR")
+            logging.debug(f"{p.id} has {len(unmatched_tags)} Flickr tags not in RR")
+            data = [(int(p.id), int(image_idx), flkr_cat, t) for t in unmatched_tags]
+            cur.executemany("INSERT INTO tags VALUES (?, ?, ?, ?)", data)
+            con.commit()
 
         # Remove tags from Flickr
         sherlocknet_tags = [t for t in p.tags if "sherlocknet" in t.text]
         logging.debug(f"Removing tags from {p.id}")
         for st in sherlocknet_tags:
             try:
-                # st.remove()  # commented out for safety during testing
                 logging.debug(f"Removing tag id={st.id} author={st.author.id} raw={st.raw}")
+                st.remove()  # comment out for safety during testing
             except:
                 logging.error(f"Failed to remove {st.id}")
+                logging.error(f"{repr(sys.exception())}")
 
-# TODO serialise saved tags as json in same format as research repository
+con.close()
